59400a5d602f282d00f1461039918a030fb96622
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-05-28 16:10:46 +1000

    Added packages for making a db. Created a db in database.py. Created a model for Problem. Finalised questions to be retreived from leetcode GraphQL.

--- services/problems/models/database.py
+++ services/problems/models/database.py
@@ -1,1 +1,19 @@
 # Setting up the db
+
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# URL = "postgresql://user:password@localhost/dbname" # for locally hosted db
+# URL = "postgresql://user:password@database/dbname" # for cotainer db
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+if not SQLALCHEMY_DATABASE_URI:
+    raise RuntimeError("SQLALCHEMY_DATABASE_URI is not set in environment...")
+
+engine = create_async_engine(SQLALCHEMY_DATABASE_URI, echo=True, pool_size=15, max_overflow=15)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
--- services/problems/models/dbmodels.py
+++ services/problems/models/dbmodels.py
@@ -1,1 +1,15 @@
 # schemas of DB
+
+from models import Base
+from sqlalchemy import Column, String, DateTime, Text
+from sqlalchemy.dialects.postgresql import ARRAY
+from datetime import datetime
+
+class Problems(Base): 
+    __tablename__ = 'Problem_Sets'
+    problem_id = Column(String, primary_key=True)
+    description = Column(Text, nullable=False)
+    topics = Column(ARRAY(Text), nullable=False)
+    difficulty = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), nullable=False, default=datetime.now())
+
