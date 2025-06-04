5b0d9424df579eb0f3cb5e6d7bd3360ac75f790d
mitsudz <MittunS2003@gmail.com>
2025-06-04 14:47:44 +1000

    initial execution service infrastructure

---
+++ infrastructure/terraform/execution.tf
@@ -0,0 +1,236 @@
+############################################################################
+# Docker Images
+resource "docker_image" "ExecutionPythonImage" {
+  name = "${aws_ecr_repository.OpenJudgeECR.repository_url}:execution:python:latest"
+  build {
+    context    = "../../services/execution"
+    dockerfile = "../../infrastructure/docker/execution/Dockerfile.python"
+  }
+}
+
+resource "docker_image" "ExecutionJavaImage" {
+  name = "${aws_ecr_repository.OpenJudgeECR.repository_url}:execution:java:latest"
+  build {
+    context    = "../../services/execution"
+    dockerfile = "../../infrastructure/docker/execution/Dockerfile.java"
+  }
+}
+
+resource "docker_registry_image" "ExecutionPythonImageName" {
+  name = docker_image.ExecutionPythonImage.name
+}
+
+resource "docker_registry_image" "ExecutionJavaImageName" {
+  name = docker_image.ExecutionJavaImage.name
+}
+
+############################################################################
+# Security Group (No External Network Access)
+resource "aws_security_group" "ExecutionSecurityGroup" {
+  name        = "ExecutionSecurityGroup"
+  description = "Execution Security Group Blocking External Input/Output"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = []
+    description = "No inbound traffic allowed"
+  }
+
+  # TODO - SET UP SO ONLY SQS TRAFFIC ALLOWED!
+}
+
+############################################################################
+# ECS Cluster
+resource "aws_ecs_cluster" "ExecutionServiceCluster" {
+  name = "ExecutionServiceCluster"
+}
+
+# Python
+resource "aws_ecs_service" "ExecutionPythonService" {
+  name            = "ExecutionPythonService"
+  cluster         = aws_ecs_cluster.ExecutionServiceCluster.id
+  task_definition = aws_ecs_task_definition.ExecutionPythonTask.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  depends_on = [
+    # TODO - ADD SQS QUEUES AND CORRECT DOCKER REGISTRY IMAGE IN HERE!
+    docker_registry_image.ExecutionPythonImageName,
+    # SQS QUEUE (OUTPUT AND PYTHON)
+  ]
+
+  network_configuration {
+    subnets          = data.aws_subnets.private.ids
+    security_groups  = [aws_security_group.ExecutionSecurityGroup.id]
+    assign_public_ip = false
+  }
+}
+
+# TODO - SET ALL ENVIRONMENT VARIABLES!!!
+resource "aws_ecs_task_definition" "ExecutionPythonTask" {
+  # Explicitly set linux for sandboxing
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+
+  family                   = "ExecutionPython"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 2048
+  memory                   = 4096
+  execution_role_arn       = data.aws_iam_role.lab.arn
+  task_role_arn            = data.aws_iam_role.lab.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "ExecutionPythonTask"
+      image     = "${aws_ecr_repository.OpenJudgeECR.repository_url}:execution:python:latest"
+      essential = true
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ExecutionPython"
+          "awslogs-region"        = var.AWS_REGION
+          "awslogs-stream-prefix" = "ecs"
+          "awslogs-create-group"  = "true"
+        }
+      }
+      environment = [
+        {
+          name  = "ENV"
+          value = "production"
+        },
+      ]
+    }
+  ])
+}
+
+# Java
+resource "aws_ecs_service" "ExecutionJavaService" {
+  name            = "ExecutionJavaService"
+  cluster         = aws_ecs_cluster.ExecutionServiceCluster.id
+  task_definition = aws_ecs_task_definition.ExecutionJavaTask.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  depends_on = [
+    # TODO - ADD SQS QUEUES AND CORRECT DOCKER REGISTRY IMAGE IN HERE!
+    docker_registry_image.ExecutionJavaImageName,
+    # SQS QUEUE (OUTPUT AND PYTHON)
+  ]
+
+  network_configuration {
+    subnets          = data.aws_subnets.private.ids
+    security_groups  = [aws_security_group.ExecutionSecurityGroup.id]
+    assign_public_ip = false
+  }
+}
+
+# TODO - SET ALL ENVIRONMENT VARIABLES!!!
+resource "aws_ecs_task_definition" "ExecutionJavaTask" {
+  # Explicitly set linux for sandboxing
+  runtime_platform {
+    cpu_architecture        = "X86_64"
+    operating_system_family = "LINUX"
+  }
+
+  family                   = "ExecutionJava"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 2048
+  memory                   = 4096
+  execution_role_arn       = data.aws_iam_role.lab.arn
+  task_role_arn            = data.aws_iam_role.lab.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "ExecutionPythonTask"
+      image     = "${aws_ecr_repository.OpenJudgeECR.repository_url}:execution:java:latest"
+      essential = true
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ExecutionJava"
+          "awslogs-region"        = var.AWS_REGION
+          "awslogs-stream-prefix" = "ecs"
+          "awslogs-create-group"  = "true"
+        }
+      }
+      environment = [
+        {
+          name  = "ENV"
+          value = "production"
+        },
+      ]
+    }
+  ])
+}
+
+############################################################################
+# Autoscaling
+
+# Python
+resource "aws_appautoscaling_target" "ExecutionPythonAutoScalingTarget" {
+  max_capacity       = 3
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.ExecutionServiceCluster.name}/${aws_ecs_service.ExecutionPythonService.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ExecutionPythonAutoScalingPolicy" {
+  name               = "ExecutionPythonAutoScalingPolicy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ExecutionPythonAutoScalingTarget.resource_id
+  scalable_dimension = aws_appautoscaling_target.ExecutionPythonAutoScalingTarget.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ExecutionPythonAutoScalingTarget.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 50.0
+    scale_out_cooldown = 60
+  }
+}
+
+# Java
+resource "aws_appautoscaling_target" "ExecutionJavaAutoScalingTarget" {
+  max_capacity       = 3
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.ExecutionServiceCluster.name}/${aws_ecs_service.ExecutionJavaService.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ExecutionJavaAutoScalingPolicy" {
+  name               = "ExecutionJavaAutoScalingPolicy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ExecutionJavaAutoScalingTarget.resource_id
+  scalable_dimension = aws_appautoscaling_target.ExecutionJavaAutoScalingTarget.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ExecutionJavaAutoScalingTarget.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 50.0
+    scale_out_cooldown = 60
+  }
+}
+
+############################################################################
+# Output
+resource "null_resource" "summary" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "==== OpenJudge Execution Deployment Complete! ===="
+      echo "Execution Image Repo URL: ${aws_ecr_repository.OpenJudgeECR.repository_url}"
+      echo ""
+    EOT
+  }
+}
+############################################################################
