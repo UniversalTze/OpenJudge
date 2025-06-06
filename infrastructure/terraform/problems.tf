fac7331efdafbd0b02ff3ec402183d8b021dffae
mitsudz <MittunS2003@gmail.com>
2025-06-06 21:29:40 +1000

    added autoscaling to problems

--- infrastructure/terraform/problems.tf
+++ infrastructure/terraform/problems.tf
@@ -210,7 +210,32 @@ resource "aws_security_group" "ProblemAPILoadBalancerSecurityGroup" {
 }
 
 ############################################################################
-# Autoscaling TODO
+# Autoscaling
+
+resource "aws_appautoscaling_target" "ProblemsAPIAutoScalingTarget" {
+  max_capacity       = 3
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.open-judge-cluster.name}/${aws_ecs_service.ProblemsAPI.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ProblemsAPIAutoScalingPolicy" {
+  name               = "ProblemsAPIAutoScalingPolicy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ProblemsAPIAutoScalingTarget.resource_id
+  scalable_dimension = aws_appautoscaling_target.ProblemsAPIAutoScalingTarget.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ProblemsAPIAutoScalingTarget.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 50.0
+    scale_out_cooldown = 60
+  }
+}
+
 
 ############################################################################
 # Output
