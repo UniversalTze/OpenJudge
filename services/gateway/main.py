c14e936bd44edcdd2784fa174b40c2016c661b55
mitsudz <MittunS2003@gmail.com>
2025-06-08 17:33:00 +1000

    commented out wrong middleware

--- services/gateway/main.py
+++ services/gateway/main.py
@@ -35,17 +35,17 @@ app.middleware("http")(rate_limit_middleware)
 app.middleware("http")(authorise_request)
 
 # Security middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"] if config.ENV == "local" else [config.FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # TODO - ADD BACK IN HTTPS WHEN IMPLEMENTED
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"] if config.ENV == "local" else [config.FRONTEND_URL],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 if config.ENV != "local":
     app.add_middleware(TrustedHostMiddleware, allowed_hosts=[config.FRONTEND_URL])
-    app.add_middleware(HTTPSRedirectMiddleware)
+    # app.add_middleware(HTTPSRedirectMiddleware)
 
 # Health check endpoints
 @app.get("/health")
